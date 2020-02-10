349253078c9fc61dd5caa140dfd1b6bcaa00a46d
Cryptomental <cryptomental.com@gmail.com>
2020-02-10 20:26:28 +0100

    shuffle: Add prysm shuffle fuzz go part and the header file.
    Cherry picked from initial work by gnattishness.

---
+++ files/fuzzers/shuffle/prysm/fuzz.go
@@ -0,0 +1,35 @@
+package fuzz
+
+import (
+	"encoding/binary"
+	"fmt"
+
+	"github.com/prysmaticlabs/prysm/beacon-chain/core/helpers"
+)
+
+func Fuzz(data []byte) []byte {
+	if len(data) < 32+2 {
+		return []byte{}
+	}
+
+	count := int(binary.LittleEndian.Uint16(data[:2])) % 100
+	var seed [32]byte
+	copy(seed[:], data[2:34])
+
+	input := make([]uint64, count)
+	for i := 0; i < count; i++ {
+		input[i] = uint64(i)
+	}
+
+	input, err := helpers.UnshuffleList(input, seed)
+	if err != nil {
+		panic(fmt.Sprintf("Unshuffling failed with: %v", err))
+	}
+
+	ret := make([]byte, count*8)
+	for i := 0; i < count; i++ {
+		binary.LittleEndian.PutUint64(ret[i*8:], uint64(input[i]))
+	}
+
+	return ret
+}
